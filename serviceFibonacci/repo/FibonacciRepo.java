67dcb445eb447c03b708a70581f3672a0682155e
Padmini-B <102512696+Padmini-B@users.noreply.github.com>
2023-04-10 20:33:42 +0530

    Delete serviceFibonacci/repo directory

--- serviceFibonacci/repo/FibonacciRepo.java
+++
@@ -1,9 +0,0 @@
-package com.docker.serviceFibonacci.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.docker.serviceFibonacci.entity.Fibonacci;
-
-public interface FibonacciRepo extends JpaRepository <Fibonacci, Integer>{
-
-}
